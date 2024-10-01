18a26858f08a51562b4a1885b1ae4d6374713d3b
wandererbayer <60208886+wandererbayer@users.noreply.github.com>
2024-10-01 20:58:49 -0300

    PUT

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ app.get('/visualizar', (req, res) =>{
 })
 
 app.delete('/params/:id', (req, res) =>{
-    const {id} = req.params
+    const {id} = req.params //pegar o id(índice da lista)
     personList.splice(id, 1)
     res.send(` ${id} removido.`)
 })
+
+app.put('/atualizar/:id', (req, res) =>{ //https://www.geeksforgeeks.org/express-js-app-put-function/?ref=gcse_ind
+    const {id} = req.params //pega o id
+    const {name, age} = req.body
+
+    if (personList[id]) {
+        personList[id] = {name, age} //atualiza o nome e a idade
+        res.send(`Usuário ${id} atualizado com sucesso.`) 
+    }
+    else
+    {
+        res.send('Usuário não encontrado.')
+    }
+}) //* testar no postman *//
 
 app.post("/cadastrar", (req, res) =>{ //método http post | ("/") = endpoint
     const {name, age} = req.body
