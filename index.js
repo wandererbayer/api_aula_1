b88fa2b1fb7cbdb0c1e5e35681de9e9200d600cd
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 17:24:52 -0300

    Node Post e Delete

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
-const app = express()
+const app = express() //usar express
 
-const port = 3000
+app.use(express.json()) //usar o express para requisição post
 
-app.get('/', (req, res) =>{
-    res.send("Fuck Society!")
+let personList = []
+
+const port = 3000 //definição da porta
+const nome = "Lucifer"
+
+app.get('/visualizar', (req, res) =>{
+    res.send(personList)
 })
 
-app.listen(port, () =>{
+app.delete('/params/:id', (req, res) =>{
+    const {id} = req.params
+    personList.splice(id, 1)
+    res.send(` ${id} removido.`)
+})
+
+app.post("/cadastrar", (req, res) =>{ //método http post | ("/") = endpoint
+    const {name, age} = req.body
+    personList.push({name, age})
+    res.send(`Usuário recebido!!! ${name}`) //resposta
+})
+
+app.listen(port, () =>{ //"escutar" a porta
     console.log(`Example app listening on port ${port}`)
 })
+
+console.log("Hell!")
